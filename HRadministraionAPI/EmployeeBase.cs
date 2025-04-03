5fcc9b028945e3a9d70af0431e5bdafd702d8cfb
janidu22 <janidudhakshi222@gmail.com>
2025-04-03 22:33:12 +0530

    implimented a basic HRAdministration Api for emplyee records

---
+++ HRadministraionAPI/EmployeeBase.cs
@@ -0,0 +1,20 @@
+﻿using HRAdministrationAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdminstrationAPI
+{
+    public class EmployeeBase : IEmployee
+    {
+        public int Id { get; set; }
+
+        public required string FirstName { get; set; }
+
+        public  required string LastName { get; set; }
+
+        public virtual decimal Salary { get; set; }
+    }
+}
---
+++ HRadministraionAPI/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationAPI
+{
+    interface IEmployee
+    {
+        int Id { get; set; }
+        string FirstName { get; }
+        string LastName { get; }
+        decimal Salary { get; set; }
+    }
+}
