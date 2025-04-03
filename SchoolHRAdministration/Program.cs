cac86843059cc9f3b1dec123ab3a6f8a279fb22b
janidu22 <janidudhakshi222@gmail.com>
2025-04-03 22:14:14 +0530

    Add project files.

---
+++ SchoolHRAdministration/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolHRAdministration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
